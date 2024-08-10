bee95ba933c86a2b17108fc59930bcc52b4f181b
naoji3x <naoji.3x@gmail.com>
2024-08-10 01:48:26 +0000

    add lint and prettier

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,13 +7,13 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-import * as logger from "firebase-functions/logger";
-import { onRequest } from "firebase-functions/v2/https";
+import * as logger from 'firebase-functions/logger'
+import { onRequest } from 'firebase-functions/v2/https'
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
 
 export const helloWorld = onRequest((request, response) => {
-  logger.info("Hello logs!", { structuredData: true });
-  response.send("Hello from Firebase!");
-});
+  logger.info('Hello logs!', { structuredData: true })
+  response.send('Hello from Firebase!')
+})
