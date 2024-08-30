c38656fd534c46c817fac07cbeb6a4274c640f90
naoji3x <naoji.3x@gmail.com>
2024-08-30 14:39:06 +0000

    feat: redirect to signin on error

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -3,5 +3,5 @@ import Google from 'next-auth/providers/google'
 
 export default {
   providers: [Google],
-  pages: { error: '/', signIn: '/signin', signOut: '/signout' }
+  pages: { error: '/signin', signIn: '/signin', signOut: '/signout' }
 } satisfies NextAuthConfig
