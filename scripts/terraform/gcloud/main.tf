926e150895e2985307acd74cee3d5d4e638eab4f
naoji3x <naoji.3x@gmail.com>
2024-09-01 12:43:56 +0000

    build: add service account creation scripts

--- scripts/terraform/gcloud/main.tf
+++ scripts/terraform/gcloud/main.tf
@@ -3,43 +3,44 @@ provider "google" {
   region  = var.region
 }
 
-resource "google_service_account" "github_actions_admin" {
-  account_id   = "github-actions-admin"
-  display_name = "GitHub Actions Admin Service Account"
+resource "google_service_account" "github_actions_sa" {
+  account_id   = var.account_id
+  display_name = "GitHub Actions Service Account"
+  description  = "Service account for GitHub Actions"
 }
 
 resource "google_project_iam_member" "artifact_registry_admin" {
   project = var.project_id
-  role    = "roles/artifactregistry.admin"
-  member  = "serviceAccount:${google_service_account.github_actions_admin.email}"
+  role    = "roles/artifactregistry.repoAdmin"
+  member  = "serviceAccount:${google_service_account.github_actions_sa.email}"
 }
 
 resource "google_project_iam_member" "cloud_build_service_account" {
   project = var.project_id
   role    = "roles/cloudbuild.builds.builder"
-  member  = "serviceAccount:${google_service_account.github_actions_admin.email}"
+  member  = "serviceAccount:${google_service_account.github_actions_sa.email}"
 }
 
 resource "google_project_iam_member" "cloud_runtimeconfig_admin" {
   project = var.project_id
-  role    = "roles/cloudtasks.enqueuer"
-  member  = "serviceAccount:${google_service_account.github_actions_admin.email}"
+  role    = "roles/cloudtasks.taskRunner"
+  member  = "serviceAccount:${google_service_account.github_actions_sa.email}"
 }
 
 resource "google_project_iam_member" "cloud_tasks_task_executor" {
   project = var.project_id
   role    = "roles/runtimeconfig.admin"
-  member  = "serviceAccount:${google_service_account.github_actions_admin.email}"
+  member  = "serviceAccount:${google_service_account.github_actions_sa.email}"
 }
 
 resource "google_project_iam_member" "firebase_admin" {
   project = var.project_id
   role    = "roles/firebase.admin"
-  member  = "serviceAccount:${google_service_account.github_actions_admin.email}"
+  member  = "serviceAccount:${google_service_account.github_actions_sa.email}"
 }
 
 resource "google_project_iam_member" "service_account_user" {
   project = var.project_id
   role    = "roles/iam.serviceAccountUser"
-  member  = "serviceAccount:${google_service_account.github_actions_admin.email}"
+  member  = "serviceAccount:${google_service_account.github_actions_sa.email}"
 }
