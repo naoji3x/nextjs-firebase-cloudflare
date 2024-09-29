d6210980ccaee9da694dda5515c213bbfa855355
naoji3x <naoji.3x@gmail.com>
2024-09-29 10:39:52 +0000

    fix: remove listener from firebase-messaging-sw

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,15 +1,27 @@
 'use strict'
 
-self.addEventListener('push', (event) => {
-  const payload = event.data.json()
+// eslint-disable-next-line no-undef
+importScripts(
+  'https://www.gstatic.com/firebasejs/10.13.2/firebase-app-compat.js'
+)
+// eslint-disable-next-line no-undef
+importScripts(
+  'https://www.gstatic.com/firebasejs/10.13.2/firebase-messaging-compat.js'
+)
 
-  const notificationTitle = payload.notification.title
-  const notificationOptions = {
-    body: payload.notification.body,
-    icon: payload.notification.icon
-  }
+// eslint-disable-next-line no-undef
+importScripts('./sw-env.js')
 
-  event.waitUntil(
-    self.registration.showNotification(notificationTitle, notificationOptions)
-  )
-})
+// こちらはブラウザでバックグラウンドで通知を受け取るためのファイルです。
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID
+}
+
+firebase.initializeApp(firebaseConfig)
+const messaging = firebase.messaging()
