772f6b2f8ba4733c466646c9b65797540c8c89d6
naoji3x <naoji.3x@gmail.com>
2024-10-12 13:47:10 +0000

    fix: restore firebase-messaging-sw.js

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,39 +1,27 @@
 'use strict'
 
-importScripts('https://www.gstatic.com/firebasejs/8.5.0/firebase-app.js')
-importScripts('https://www.gstatic.com/firebasejs/8.5.0/firebase-messaging.js')
+// eslint-disable-next-line no-undef
+importScripts(
+  'https://www.gstatic.com/firebasejs/10.14.1/firebase-app-compat.js'
+)
+// eslint-disable-next-line no-undef
+importScripts(
+  'https://www.gstatic.com/firebasejs/10.14.1/firebase-messaging-compat.js'
+)
 
 // eslint-disable-next-line no-undef
 importScripts('./sw-env.js')
-console.log('NEXT_PUBLIC_API_KEY = ' + process.env.NEXT_PUBLIC_API_KEY)
 
 // こちらはブラウザでバックグラウンドで通知を受け取るためのファイルです。
 
-if (!firebase.apps.length) {
-  const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_APP_ID
-  }
-  firebase.initializeApp(firebaseConfig)
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID
 }
 
-firebase.messaging()
-
-//background notifications will be received here
-firebase.messaging().onBackgroundMessage(async (message) => {
-  if (Notification.permission === 'granted') {
-    if (navigator.serviceWorker)
-      navigator.serviceWorker.getRegistration().then(async function (reg) {
-        if (reg) {
-          // firebase.message().useServiceWorker(reg)
-          await reg.showNotification(message.notification.title, {
-            body: message.notification.body
-          })
-        }
-      })
-  }
-})
+firebase.initializeApp(firebaseConfig)
+const messaging = firebase.messaging()
