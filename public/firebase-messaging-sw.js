7b4dbdc7b81c327a11cbf69b4137afa8e9a35e28
naoji3x <naoji.3x@gmail.com>
2024-10-12 07:58:24 +0000

    fix: downgrade firebase.js to v8.5 in firebase-messaging-sw.js

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,27 +1,38 @@
 'use strict'
+//Firebase Messaging
+// import firebase from "firebase/app";
 
-// eslint-disable-next-line no-undef
-importScripts(
-  'https://www.gstatic.com/firebasejs/10.14.0/firebase-app-compat.js'
-)
-// eslint-disable-next-line no-undef
-importScripts(
-  'https://www.gstatic.com/firebasejs/10.14.0/firebase-messaging-compat.js'
-)
+importScripts('https://www.gstatic.com/firebasejs/8.5.0/firebase-app.js')
+importScripts('https://www.gstatic.com/firebasejs/8.5.0/firebase-messaging.js')
 
 // eslint-disable-next-line no-undef
 importScripts('./sw-env.js')
 
 // こちらはブラウザでバックグラウンドで通知を受け取るためのファイルです。
 
-const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_APP_ID
+if (!firebase.apps.length) {
+  const firebaseConfig = {
+    apiKey: process.env.NEXT_PUBLIC_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_APP_ID
+  }
+  firebase.initializeApp(firebaseConfig)
 }
 
-firebase.initializeApp(firebaseConfig)
-const messaging = firebase.messaging()
+firebase.messaging()
+
+//background notifications will be received here
+firebase.messaging().onBackgroundMessage(async (message) => {
+  if (Notification.permission === 'granted') {
+    if (navigator.serviceWorker)
+      navigator.serviceWorker.getRegistration().then(async function (reg) {
+        if (reg)
+          await reg.showNotification(message.notification.title, {
+            body: message.notification.body
+          })
+      })
+  }
+})
